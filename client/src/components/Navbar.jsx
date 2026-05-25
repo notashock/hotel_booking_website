ee598043b4f7ec8138f8b13338513d21184af11e
kalyani-cherukuri <venkatanagasaikalyani_cherukuri@srmap.edu.in>
2026-05-25 12:24:46 +0530

    Added Routes and Login.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+
+import { useAuth } from "../context/AuthContext";
+
+const Navbar = () => {
+
+  const { user, logout } = useAuth();
+
+  return (
+
+    <div className="bg-blue-600 text-white px-8 py-4 flex justify-between">
+
+      <h1 className="text-2xl font-bold">
+        Hotel Booking
+      </h1>
+
+      <div className="flex gap-5 items-center">
+
+        <Link to="/">Home</Link>
+
+        <Link to="/hotels">
+          Hotels
+        </Link>
+
+        {
+          user?.role === "ADMIN" && (
+            <Link to="/admin">
+              Admin
+            </Link>
+          )
+        }
+
+        {
+          user?.role === "RECEPTIONIST" && (
+            <Link to="/reception">
+              Reception
+            </Link>
+          )
+        }
+
+        {
+          user ? (
+            <button
+              onClick={logout}
+              className="bg-red-500 px-4 py-2 rounded"
+            >
+              Logout
+            </button>
+          ) : (
+            <Link to="/login">
+              Login
+            </Link>
+          )
+        }
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
